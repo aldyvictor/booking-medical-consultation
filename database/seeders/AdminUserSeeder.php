4c1fa58c09a3777c003298532e03c117faf90018
aldyvictor <aldyvictor1046@gmail.com>
2022-06-18 19:39:12 +0700

    feat(controller, seeder, view): New feature login for user customer and admin

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'Admin Clinic',
+            'email' => 'adminclinic1@gmail.com',
+            'password' => bcrypt('admin123'),
+            'role' => 'Admin',
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Form Login Admin
+Route::get('/login-admin', [App\Http\Controllers\Auth\LoginController::class, 'showLoginFormAdmin'])->name('login-admin');
